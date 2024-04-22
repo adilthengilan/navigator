51d29e18debb3fbb6e5929973576f8e599470701
NaheelJ <naheeljnaheel5454@gmail.com>
2024-04-22 17:05:50 +0530

    Mobile view updated

--- lib/view/mobile_view/Contact/contact.dart
+++ lib/view/mobile_view/Contact/contact.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:navigator_portal/model/widgets/constant.dart';
 
 class MvContact extends StatefulWidget {
   final double height;
@@ -17,36 +16,8 @@ class MvContact extends StatefulWidget {
 class _ContactState extends State<MvContact> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width: widget.width,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            sizedBox(20, 0),
-            Image.asset(
-              'assets/images/logobluetext.png',
-              width: 180,
-            ),
-            sizedBox(20, 0),
-            Row(
-              children: [
-                for (int i = 0; i < 4; i++)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: CircleAvatar(
-                      radius: 13,
-                      backgroundImage: AssetImage("assets/images/whatsapp_icon.png"),
-                    ),
-                  )
-              ],
-            ),
-            sizedBox(100, 0)
-          ],
-        ),
-      ),
+    return Column(
+      children: [],
     );
   }
 }
--- lib/view/mobile_view/dashBoard/Widgets/Drawer/student_reference.dart
+++ lib/view/mobile_view/dashBoard/Widgets/Drawer/student_reference.dart
@@ -1,77 +1,77 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void studentProviderShowdialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Student Detailes'),
-        content: Column(children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Student Name",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Location(optional)",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Looking Course",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "College(optional)",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Phone Number",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-            ),
-          ),
-        ]),
-        actions: [
-          TextButton(
-            onPressed: () {
-              // Close the dialog
-              Navigator.of(context).pop();
-            },
-            child: Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              // Close the dialog
-              Navigator.of(context).pop();
-            },
-            child: Text('Submit'),
-          ),
-        ],
-      );
-    },
-  );
-}
+// void studentProviderShowdialog(BuildContext context) {
+//   showDialog(
+//     context: context,
+//     builder: (BuildContext context) {
+//       return AlertDialog(
+//         title: Text('Student Detailes'),
+//         content: Column(children: [
+//           TextField(
+//             decoration: InputDecoration(
+//               labelText: "Student Name",
+//               border:
+//                   OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 15,
+//           ),
+//           TextField(
+//             decoration: InputDecoration(
+//               labelText: "Location(optional)",
+//               border:
+//                   OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 15,
+//           ),
+//           TextField(
+//             decoration: InputDecoration(
+//               labelText: "Looking Course",
+//               border:
+//                   OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 15,
+//           ),
+//           TextField(
+//             decoration: InputDecoration(
+//               labelText: "College(optional)",
+//               border:
+//                   OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 15,
+//           ),
+//           TextField(
+//             decoration: InputDecoration(
+//               labelText: "Phone Number",
+//               border:
+//                   OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+//             ),
+//           ),
+//         ]),
+//         actions: [
+//           TextButton(
+//             onPressed: () {
+//               // Close the dialog
+//               Navigator.of(context).pop();
+//             },
+//             child: Text('Cancel'),
+//           ),
+//           TextButton(
+//             onPressed: () {
+//               // Close the dialog
+//               Navigator.of(context).pop();
+//             },
+//             child: Text('Submit'),
+//           ),
+//         ],
+//       );
+//     },
+//   );
+// }
